2d392ab8dffde16c2414b17e9d6036027d257d61
MitchellMcMaugh <mitchellmcmaugh@gmail.com>
2024-11-19 19:51:23 +1100

    DB change

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -14,7 +14,7 @@ export default ({ env }) => {
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
+          ca: env('DATABASE_SSL_CA', env('CA_CERT', undefined)), // Added CA_CERT as a fallback
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
@@ -33,7 +33,7 @@ export default ({ env }) => {
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
+          ca: env('DATABASE_SSL_CA', env('CA_CERT', undefined)), // Added CA_CERT as a fallback
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
